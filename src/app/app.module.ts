3e5852b7f0c67c874443fc2fe6784dff5242d51e
pfuentesg <pablofuentesg1@yahoo.es>
2017-10-23 12:28:37 +0200

    add components an routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {MarvelApiService} from './services/marvel-api.service';
+import { ListComponent } from './list/list.component';
+import { HeaderComponent } from './header/header.component'
 
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',  component: ListComponent },
+ 
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [MarvelApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/marvel-api.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MarvelApiService {
+
+  constructor() { }
+
+AskForHeroes() {
+  console.log("ok")
+}
+
+
+}
